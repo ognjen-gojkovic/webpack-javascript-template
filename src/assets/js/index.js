4dc2150bb19f811a5ca5dec3769185817bc8fa60
Ognjen Gojković <ognjen1804@gmail.com>
2020-05-14 12:45:40 +0200

    updated

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,11 @@
-import "../css/style.css";
+import style from "../css/style.module.css";
 
-const title = document.createElement("h2");
-title.textContent = "Hello from Webpack";
-document.body.appendChild(title);
+const app = document.createElement("div");
+console.log(style.app);
+app.className = `${style.app}`;
+app.textContent = "Hello from Webpack";
+
+document.body.appendChild(app);
+
+//=========================================================================================
+//=========================================================================================
